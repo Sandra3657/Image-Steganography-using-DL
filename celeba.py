f6a2e99281d94f8bdb0c4fa191005717ba54b2c8
Sandra K <sandrakakkarayil01@gmail.com>
2022-12-10 15:41:03 +0530

    Modification in celeba code

--- celeba.py
+++ celeba.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 import numpy as np
 
-def get_celeba(batch_size, dataset_directory, split=0.2):
+def get_celeba(batch_size, dataset_directory, train_val_split=0.8):
     # 1. Download this file into dataset_directory and unzip it:
     #  https://drive.google.com/open?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM
     # 2. Put the `img_align_celeba` directory into the `celeba` directory!
@@ -24,22 +24,30 @@ def get_celeba(batch_size, dataset_directory, split=0.2):
     dataset = torch.utils.data.Subset(dataset, np.random.choice(len(dataset), 50000, replace=False))
     
     n = len(dataset)
-    print("No. of images: ",n)
-    test_split = split/2.0
+    # test_split = train_val_split/2.0
 
-    n_test = int(n*test_split)
-    n_train = int((n-2*n_test)/2)
+    # n_test = int(n*test_split)
+    # n_train = int((n-2*n_test)/2)
+    n_test = int(5000/2)
+    n_train_val = int((n-5000))
+    n_train = int(train_val_split*n_train_val/2)
+    n_val = int((n_train_val-(2*n_train))/2)
     print("No. of training imgs:", 2*n_train)
+    print("No. of validation imgs:", 2*n_val)
     print("No. of testing imgs:", 2*n_test)
+    source_train, payload_train, source_test, payload_test, source_val, payload_val = torch.utils.data.random_split(
+    dataset, (n_train, n_train, n_test, n_test, n_val, n_val))
 
-    source_train, payload_train, source_test, payload_test = torch.utils.data.random_split(
-    dataset, (n_train, n_train, n_test, n_test))
-    print(source_train)
     # Prepare Data Loaders for training and validation
     source_train_loader = torch.utils.data.DataLoader(source_train, batch_size=batch_size, shuffle=True)
     source_test_loader = torch.utils.data.DataLoader(source_test, shuffle=True)
+    source_val_loader = torch.utils.data.DataLoader(source_val, shuffle=True)
     payload_train_loader = torch.utils.data.DataLoader(payload_train, batch_size=batch_size, shuffle=True)
     payload_test_loader = torch.utils.data.DataLoader(payload_test, shuffle=True)
+    payload_val_loader = torch.utils.data.DataLoader(payload_val, shuffle=True)
+    
 
 
-    return (source_train_loader, payload_train_loader) , (source_test_loader, payload_test_loader)
+    return (source_train_loader, payload_train_loader) , (source_test_loader, payload_test_loader), (source_val_loader, payload_val_loader)
+
+train_loader, test_loader, val_loader = get_celeba(64, "./dataset/")
